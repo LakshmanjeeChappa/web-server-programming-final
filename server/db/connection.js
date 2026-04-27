3a2f16b961ae2de83b9f83913d989b60643dd6ad
LakshmanjeeChappa <chappal1@newpaltz.edu>
2026-04-27 18:39:33 -0400

    final setup

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,15 +1,12 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = pool;
+
+console.log("DB CONFIG:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT
+});
