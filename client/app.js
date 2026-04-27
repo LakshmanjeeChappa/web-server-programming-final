077da81449feb09569458d27aa9fa415b434277c
LakshmanjeeChappa <chappal1@newpaltz.edu>
2026-04-27 14:30:49 -0400

    replaced api url

--- client/app.js
+++ client/app.js
@@ -14,3 +14,11 @@ const app = Vue.createApp({})
 app.use(router)
 
 app.mount("#app")
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
