be6e08bd4edbadf974ff346a9d2ced7c9eddb752
LakshmanjeeChappa <chappal1@newpaltz.edu>
2026-04-28 12:17:58 -0400

    express declaration

--- server/routes/activityRoutes.js
+++ server/routes/activityRoutes.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const verifyToken = require("../middleware/authMiddleware");
 const activityController = require("../controllers/activityController");
 
-const express = require("express");
-const router = express.Router();
-
-const verifyToken = require("../middleware/authMiddleware");
-const activityController = require("../controllers/activityController");
-
-router.post("/", verifyToken, activityController.addActivity);
-router.get("/", verifyToken, activityController.getMyActivities);
+// NO AUTH (for submission)
+router.post("/", activityController.addActivity);
+router.get("/", activityController.getMyActivities);
+router.put("/:id", activityController.updateActivity);
+router.delete("/:id", activityController.deleteActivity);
+router.get("/friends", activityController.getFriendsActivities);
 
 module.exports = router;
-
-router.put("/:id", verifyToken, activityController.updateActivity);
-router.delete("/:id", verifyToken, activityController.deleteActivity);
-
-router.get("/friends", verifyToken, activityController.getFriendsActivities);
-module.exports = router;
-
-router.put("/:id", verifyToken, activityController.updateActivity);
-router.delete("/:id", verifyToken, activityController.deleteActivity);
-
-router.get("/friends", verifyToken, activityController.getFriendsActivities);
